5a566cf9eba981dc928d8b5a0c45e3b6f6426c2c
sauruskp <nino3617@outlook.com>
2022-12-18 07:35:55 +0900

    音楽なる

--- santa/santa/ContentView.swift
+++ santa/santa/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 import WebKit
 import EventKitUI
 import FirebaseFirestore
+import AVFoundation
 
 struct HomeView: View {
+    @ObservedObject var soundPlay = soundplay()
+    
     var body: some View {
         NavigationStack{
             NavigationLink{
@@ -30,8 +33,20 @@ struct HomeView: View {
                 }
             }
         }
+       
+        .onAppear(){
+            soundPlay.playAudio()
+            
+        }
+        
     }
+    
+        
 }
+
+
+
+
 
 struct AskHaveScheduleView: View {
     var body: some View {
@@ -174,114 +189,6 @@ private class WebViewURLObservable: ObservableObject {
 }
 
 
-//struct SelectSantaView: View {
-//
-//    @Environment(\.dismiss) var dismiss
-//
-//    init() {
-//        let appearance = UINavigationBarAppearance()
-//        appearance.backgroundColor = UIColor.red
-//    }
-//
-//    var body: some View {
-//        NavigationStack {
-//            ZStack {
-//                Color.xGreen
-//                    .ignoresSafeArea()
-//                Text("話を聞いてもらうサンタを選ぶ")
-//                    .foregroundColor(Color.white)
-//                    .frame(width: 395, height: 120)
-//                    .padding(.top, 30.0)
-//                    .background(Color.xRed)
-//                    .cornerRadius(20)
-//                    .padding(.bottom, 680.0)
-//                    .font(.system(size: 20))
-//                VStack {
-//                    HStack {
-//                        Circle()
-//                            .strokeBorder(Color.black)
-//                            .frame(width: 54, height: 54)
-//
-//                        VStack(alignment: .leading) {
-//                            Text("サンタ＝サン")
-//                                .fontWeight(.black)
-//                                .font(.system(size: 15))
-//                            Text("日本のクリスマスをずっと見てきた。ゆったり話を聞いてくれます。")
-//                                .font(.system(size: 13))
-//                        }
-//
-//                    }
-//                    .frame(width: 290.0, height: 54.0)
-//                    .padding(.horizontal, 18.0)
-//                    .padding(.vertical, 16.0)
-//                    .background(Color.white)
-//                    .cornerRadius(15)
-//                    .padding(.bottom, 100.0)
-//
-//                    HStack {
-//                        Circle()
-//                            .strokeBorder(Color.black)
-//                            .frame(width: 54, height: 54)
-//
-//                        VStack(alignment: .leading) {
-//                            Text("サンタ＝マン")
-//                                .fontWeight(.black)
-//                                .font(.system(size: 15))
-//                            Text("アメリカ生まれのアメリカ育ち。ファンキーに話を聞いてくれます。")
-//                                .font(.system(size: 13))
-//                        }
-//                    }
-//                    .frame(width: 290.0, height: 54.0)
-//                    .padding(.horizontal, 18.0)
-//                    .padding(.vertical, 16.0)
-//                    .background(Color.white)
-//                    .cornerRadius(15)
-//                    .padding(.bottom, 100.0)
-//
-//                    HStack {
-//                        Circle()
-//                            .strokeBorder(Color.black)
-//                            .frame(width: 54, height: 54)
-//
-//                        VStack(alignment: .leading) {
-//                            Text("サンタ＝イエア")
-//                                .fontWeight(.black)
-//                                .font(.system(size: 15))
-//
-//                            Text("世界中を飛び回っている。感情豊かに話を聞いてくれます")
-//                                .font(.system(size: 13))
-//                        }
-//                    }
-//                    .frame(width: 290.0, height: 54.0)
-//                    .padding(.horizontal, 18.0)
-//                    .padding(.vertical, 16.0)
-//                    .background(Color.white)
-//                    .cornerRadius(15)
-//                }
-//                .padding(.top, 60.0)
-//            }
-//            .navigationBarBackButtonHidden(true)
-//            .toolbar {
-//                ToolbarItemGroup(placement: .bottomBar) {
-//                    Button(
-//                        action: {
-//                            dismiss()
-//                        }, label: {
-//                            HStack {
-//                                Image(systemName: "chevron.backward")
-//                                    .foregroundColor(Color.white)
-//                                Text("本当は予定がなかった…")
-//                                    .foregroundColor(Color.white)
-//                            }
-//                        }
-//                    )
-//                    Spacer()
-//                }
-//            }
-//        }
-//    }
-//}
-
 
 struct AddCalendarEventView: View {
     @Environment(\.dismiss) var dismiss
@@ -407,8 +314,8 @@ extension View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        // HomeView()
-         AskHaveScheduleView()
+        HomeView()
+        //AskHaveScheduleView()
         // AskScheduleView()
         //AddCalendarEventView()
         // SelectSantaView()
--- santa/santa/Sound.swift
+++ santa/santa/Sound.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import AVFoundation
+
+class soundplay: NSObject, ObservableObject, AVAudioPlayerDelegate {
+    var audioPlayer: AVAudioPlayer?
+
+    func playAudio() {
+        guard let url = Bundle.main.url(forResource: "xmassongbgm", withExtension: "mp3") else { return }
+        audioPlayer = try! AVAudioPlayer(contentsOf: url)
+        audioPlayer?.delegate = self
+        audioPlayer?.play()
+       // audioPlayer?.numberOfLoops = -1 //ループ再生
+    }
+
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        print("Did finish Playing")
+    }
+}
